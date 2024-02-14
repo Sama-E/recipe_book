0954510e7f0919575b94c36b9a3fd825b8a3a4b2
Sama-E <112719147+Sama-E@users.noreply.github.com>
2024-02-13 20:01:00 -0500

    update unfinished login

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,13 +1,21 @@
 import LoginForm from "@/components/auth/login/LoginForm"
 import styles from "./login.module.css";
+import { signIn } from "@/lib/auth";
 
-const Login = () => {
+const Login = async () => {
+
+  const session = await auth();
+
+  const handleGithubLogin = async () => {
+    "use server";
+    await signIn("github");
+  }
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        {/* <form action={handleGithubLogin}>
+        <form action={handleGithubLogin}>
           <button className={styles.github}>Login with Github</button>
-        </form> */}
+        </form>
         <LoginForm />
       </div>
     </div>
